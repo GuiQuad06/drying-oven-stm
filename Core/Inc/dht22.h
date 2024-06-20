248e6758dfea09baf56809c313c7487168896fd0
Guillaume Cuadrado (GUCD) (Consultant) <gucd@oticonmedical.com>
2024-06-20 18:36:20 +0200

    Add driver for DHT22

---
+++ Core/Inc/dht22.h
@@ -0,0 +1,42 @@
+/*
+ * dht22.h
+ *
+ *  Created on: Jun 20, 2024
+ *      Author: gucd
+ */
+
+#ifndef INC_DHT22_H_
+#define INC_DHT22_H_
+
+#include <stdbool.h>
+#include <stdint.h>
+
+typedef void (*fptr_t)(void);
+typedef void (*fptr_b_t)(bool);
+typedef void (*fptr_u16_t)(uint16_t);
+typedef uint8_t (*u8_fptr_t)(void);
+
+typedef enum
+{
+    DHT22_OK    = 0,
+    DHT22_ERROR = 1
+} dht22_status_t;
+
+typedef struct
+{
+    fptr_t input_cfg;
+    fptr_b_t output_state;
+    fptr_u16_t delay_sensor;
+    u8_fptr_t input_state;
+
+} dht22_t;
+
+void dht22_init(dht22_t *device,
+                fptr_t input_cfg_cb,
+                fptr_b_t output_state_cb,
+                fptr_u16_t delay_cb,
+                u8_fptr_t input_state_cb);
+dht22_status_t dht22_start(dht22_t *device);
+uint8_t dht22_read_byte(dht22_t *device);
+
+#endif /* INC_DHT22_H_ */
---
+++ Core/Src/dht22.c
@@ -0,0 +1,82 @@
+/*
+ * dht22.c
+ *
+ *  Created on: Jun 20, 2024
+ *      Author: gucd
+ */
+#include "dht22.h"
+
+#define BYTE_LEN (8u)
+
+void dht22_init(dht22_t *device,
+                fptr_t input_cfg_cb,
+                fptr_b_t output_state_cb,
+                fptr_u16_t delay_cb,
+                u8_fptr_t input_state_cb)
+{
+    // Object config
+    device->input_cfg    = input_cfg_cb;
+    device->output_state = output_state_cb;
+    device->delay_sensor = delay_cb;
+    device->input_state  = input_state_cb;
+}
+
+dht22_status_t dht22_start(dht22_t *device)
+{
+    dht22_status_t res = DHT22_OK;
+
+    device->output_state(false);
+    device->delay_sensor(1200);
+
+    device->output_state(true);
+    device->delay_sensor(30);
+
+    device->input_cfg();
+
+    // Check answer
+    device->delay_sensor(40);
+
+    if (!device->input_state())
+    {
+        device->delay_sensor(80);
+        if (device->input_state())
+        {
+            res = DHT22_OK;
+        }
+        else
+        {
+            res = DHT22_ERROR;
+        }
+    }
+    // Wait until the pin goes low
+    while (device->input_state())
+        ;
+    return res;
+}
+
+uint8_t dht22_read_byte(dht22_t *device)
+{
+    uint8_t res = 0x00;
+
+    for (int i = 0; i < BYTE_LEN; i++)
+    {
+        // Wait until the pin goes high
+        while (!device->input_state())
+            ;
+
+        device->delay_sensor(40);
+
+        if (device->input_state())
+        {
+            res |= (1 << (BYTE_LEN - 1 - i));
+        }
+        else
+        {
+            res &= ~(1 << (BYTE_LEN - 1 - i));
+        }
+        // Wait until the pin goes low
+        while (device->input_state())
+            ;
+    }
+    return res;
+}
